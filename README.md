ab632b6e58df31b52186ac5b18d113e97a5a2dde
lkoniecki <Lukasz.Koniecki@dynatrace.com>
2021-03-05 21:27:57 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Simple example how to push metrics to Dynatrace using MINT ingest
+
+```
+pip3 freeze > requirements.txt
+```
---
+++ mint.py
@@ -0,0 +1,12 @@
+import requests, os
+
+DT_METRICS_INGEST_URL=os.environ.get('DT_METRICS_INGEST_URL')
+print(DT_METRICS_INGEST_URL)
+DT_METRIC_INGEST_TOKEN=os.environ.get('DT_METRIC_INGEST_TOKEN')
+print(DT_METRIC_INGEST_TOKEN)
+
+headers = {'Content-Type': 'text/plain',
+           'Authorization': "Api-Token " + DT_METRIC_INGEST_TOKEN}
+payload = "curl.test 1"
+r = requests.post(DT_METRICS_INGEST_URL, headers=headers, data=payload)
+print(r.text)

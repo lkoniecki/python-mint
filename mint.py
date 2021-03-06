38ab79fe744297c08af1eb1beeb397e5af382190
lkoniecki <Lukasz.Koniecki@dynatrace.com>
2021-03-06 10:00:06 +0100

    Fixed logging

--- mint.py
+++ mint.py
@@ -1,19 +1,28 @@
-import requests, os, time
+import requests
+import os
+import time
+import logging
 
-DT_METRICS_INGEST_URL=os.environ.get('DT_METRICS_INGEST_URL')
-print(DT_METRICS_INGEST_URL)
-DT_METRIC_INGEST_TOKEN=os.environ.get('DT_METRIC_INGEST_TOKEN')
-print(DT_METRIC_INGEST_TOKEN)
+def main():
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-while True:
-    headers = {'Content-Type': 'text/plain',
-            'Authorization': "Api-Token " + DT_METRIC_INGEST_TOKEN}
-    payload = "curl.test 1"
-    try:
-        r = requests.post(DT_METRICS_INGEST_URL, headers=headers, data=payload, verify=False)
-        print(r.text)
-    except requests.exceptions.RequestException as e:
-        print(e)
+    DT_METRICS_INGEST_URL = os.environ.get('DT_METRICS_INGEST_URL')
+    logging.info(DT_METRICS_INGEST_URL)
+    DT_METRIC_INGEST_TOKEN = os.environ.get('DT_METRIC_INGEST_TOKEN')
+    logging.info(DT_METRIC_INGEST_TOKEN)
 
-    print("Going sleep...")
-    time.sleep(20)
+    while True:
+        headers = {'Content-Type': 'text/plain',
+                'Authorization': "Api-Token " + DT_METRIC_INGEST_TOKEN}
+        payload = "curl.test 1"
+        try:
+            r = requests.post(DT_METRICS_INGEST_URL, headers=headers, data=payload, verify=False)
+            logging.info(r.text)
+        except Exception as e:
+            logging.error("Error sending MINT metric", exc_info=True)
+
+        logging.info("Going sleep...")
+        time.sleep(20)
+
+if __name__ == '__main__':
+    main()

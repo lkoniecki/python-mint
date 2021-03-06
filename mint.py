360a191fd7c921a6b10e7a2926a756e59c0521f9
lkoniecki <Lukasz.Koniecki@dynatrace.com>
2021-03-06 15:18:14 +0100

    Added reading Dynatrace stock price

--- mint.py
+++ mint.py
@@ -2,31 +2,56 @@ import requests
 import os
 import time
 import logging
+import json
+
+
+def read_price(symbol):
+    url = f"https://query1.finance.yahoo.com/v7/finance/chart/{symbol}?interval=5m"
+    logging.info(f"Getting {url}...")
+    try:
+        r = requests.get(url)
+        logging.info(r.text)
+        j = json.loads(r.text)
+        marketPrice = j["chart"]["result"][0]["meta"]["regularMarketPrice"]
+        logging.info(f"{symbol} is {marketPrice}")
+        return marketPrice
+    except Exception as e:
+        logging.error(f"Error reading price from {url}", exc_info=True)
+
+
+def write_metric(url, token, symbol, market_price):
+    headers = {'Content-Type': 'text/plain',
+               'Authorization': "Api-Token " + token}
+
+    payload = f"nasdaq.price,symbol={symbol} {market_price}"
+    logging.debug(f"Writing mint line: {payload}")
+    try:
+        r = requests.post(url, headers=headers, data=payload, verify=False, allow_redirects=False)
+        logging.info(r.text)
+        if r.status_code == 302:
+            redirect_url = r.headers['Location']
+            logging.info(f"Redirecting to {redirect_url}...")
+            r = requests.post(redirect_url, headers=headers, data=payload, verify=False, allow_redirects=False)
+            logging.info(r.text)
+    except Exception as e:
+        logging.error("Error sending MINT metric", exc_info=True)
+
 
 def main():
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    DT_METRICS_INGEST_URL = os.environ.get('DT_METRICS_INGEST_URL')
-    logging.info(DT_METRICS_INGEST_URL)
-    DT_METRIC_INGEST_TOKEN = os.environ.get('DT_METRIC_INGEST_TOKEN')
-    logging.info(DT_METRIC_INGEST_TOKEN)
+    dt_metric_ingest_url = os.environ.get('DT_METRICS_INGEST_URL')
+    logging.info(dt_metric_ingest_url)
+    dt_metric_ingest_token = os.environ.get('DT_METRIC_INGEST_TOKEN')
+    logging.info(dt_metric_ingest_token)
 
+    symbol = "DT"
     while True:
-        headers = {'Content-Type': 'text/plain',
-                'Authorization': "Api-Token " + DT_METRIC_INGEST_TOKEN}
-        payload = "curl.test 1"
-        try:
-            r = requests.post(DT_METRICS_INGEST_URL, headers=headers, data=payload, verify=False, allow_redirects=False)
-            logging.info(r.text)
-            if r.status_code == 302:
-                redirectUrl = r.headers['Location']
-                req2 = requests.post(redirectUrl, headers=headers, data=payload, verify=False, allow_redirects=False)
-                logging.info(req2.text)
-        except Exception as e:
-            logging.error("Error sending MINT metric", exc_info=True)
-
+        market_price = read_price(symbol)
+        write_metric(url=dt_metric_ingest_url, token=dt_metric_ingest_token, symbol=symbol, market_price=market_price)
         logging.info("Going sleep...")
-        time.sleep(20)
+        time.sleep(60)
+
 
 if __name__ == '__main__':
     main()
